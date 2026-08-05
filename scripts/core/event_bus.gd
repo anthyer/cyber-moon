008047b0dfbd99dcc947e07df0e8e922209c2d9a
anthyer <antonio_marcosph@hotmail.com>
2026-08-04 23:07:16 -0300

    Tarefa 3: Autoload EventBus
    Implementa o EventBus como um autoload global com tres sinais:
    - crop_harvested(cultivo: Cultivo, quantidade: int)
    - city_expansion_blocked(id_do_marco: String)
    - npc_relationship_changed(id_do_npc: String, novo_valor: int)
    
    Registra o autoload em project.godot para uso global pelos sistemas
    de gameplay futuros.

---
+++ scripts/core/event_bus.gd
@@ -0,0 +1,5 @@
+extends Node
+
+signal crop_harvested(cultivo: Cultivo, quantidade: int)
+signal city_expansion_blocked(id_do_marco: String)
+signal npc_relationship_changed(id_do_npc: String, novo_valor: int)
