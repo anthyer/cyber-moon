0e730f92b2fdf5494aa4e176465ec2368ae9db48
anthyer <antonio_marcosph@hotmail.com>
2026-08-04 23:21:26 -0300

    Tarefa 6: Autoload InputManager e acoes no Input Map
    Implementa o gerenciador de entrada do projeto, criando os seis eventos
    do Input Map (mover_cima, mover_baixo, mover_esquerda, mover_direita,
    interagir, abrir_inventario) programaticamente via ProjectSettings, e
    registra o InputManager como autoload para acesso global aos metodos
    obter_direcao_movimento(), interagir_pressionado() e
    abrir_inventario_pressionado().

---
+++ scripts/core/input_manager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+func obter_direcao_movimento() -> Vector2:
+	return Input.get_vector("mover_esquerda", "mover_direita", "mover_cima", "mover_baixo")
+
+func interagir_pressionado() -> bool:
+	return Input.is_action_just_pressed("interagir")
+
+func abrir_inventario_pressionado() -> bool:
+	return Input.is_action_just_pressed("abrir_inventario")
