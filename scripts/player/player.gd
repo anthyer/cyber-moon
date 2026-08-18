18a1922514fbd3d55272430cf688bbcf6d316937
anthyer <antonio_marcosph@hotmail.com>
2026-08-18 19:24:39 -0300

    feat(farming): enxada e regador substituem o combo de ataque quando equipados

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -12,6 +12,7 @@ extends CharacterBody3D
 @export var cooldown_ataque: float = 0.3
 
 const CLIPES_COMBO_ATAQUE: Array[String] = ["attack-melee-left", "attack-melee-left", "attack-melee-right"]
+const CLIPES_INTERACAO: Array[String] = ["interact-left", "interact-right"]
 
 @onready var personagem: Node3D = $Personagem
 @onready var animation_player: AnimationPlayer = $Personagem/AnimationPlayer
@@ -23,6 +24,7 @@ var _tempo_cooldown_restante: float = 0.0
 var _direcao_dash: Vector3 = Vector3.ZERO
 
 var _indice_combo: int = 0
+var _indice_interacao: int = 0
 var _tempo_ataque_restante: float = 0.0
 var _tempo_movimento_travado_ataque_restante: float = 0.0
 var _tempo_janela_combo_restante: float = 0.0
@@ -54,19 +56,28 @@ func _physics_process(delta: float) -> void:
 		_tempo_janela_combo_restante = 0.0
 
 	if _tempo_dash_restante <= 0.0 and _tempo_ataque_restante <= 0.0 and _tempo_cooldown_ataque_restante <= 0.0 and InputManager.atacar_pressionado():
-		var nome_clipe: String = CLIPES_COMBO_ATAQUE[_indice_combo]
-		animation_player.play(nome_clipe, -1.0, velocidade_ataque)
+		if ferramenta_equipada == null:
+			var nome_clipe: String = CLIPES_COMBO_ATAQUE[_indice_combo]
+			_travar_movimento_pela_animacao(nome_clipe)
+			_tempo_janela_combo_restante = janela_combo_ataque
 
-		var duracao_golpe: float = animation_player.get_animation(nome_clipe).length / velocidade_ataque
-		_tempo_ataque_restante = duracao_golpe
-		_tempo_movimento_travado_ataque_restante = duracao_golpe + folga_pos_golpe
-		_tempo_janela_combo_restante = janela_combo_ataque
+			_indice_combo += 1
+			if _indice_combo >= CLIPES_COMBO_ATAQUE.size():
+				_indice_combo = 0
+				_tempo_cooldown_ataque_restante = cooldown_ataque
+				_tempo_janela_combo_restante = 0.0
+		else:
+			var celula_alvo: Vector2i = grade_solo.obter_celula_alvo(global_position, personagem.rotation.y)
+			var acao_teve_efeito: bool = false
+			if ferramenta_equipada.id_acao == &"enxada":
+				acao_teve_efeito = grade_solo.arar(celula_alvo)
+			elif ferramenta_equipada.id_acao == &"regador":
+				acao_teve_efeito = grade_solo.molhar(celula_alvo)
 
-		_indice_combo += 1
-		if _indice_combo >= CLIPES_COMBO_ATAQUE.size():
-			_indice_combo = 0
-			_tempo_cooldown_ataque_restante = cooldown_ataque
-			_tempo_janela_combo_restante = 0.0
+			if acao_teve_efeito:
+				var nome_clipe_interacao: String = CLIPES_INTERACAO[_indice_interacao]
+				_travar_movimento_pela_animacao(nome_clipe_interacao)
+				_indice_interacao = (_indice_interacao + 1) % CLIPES_INTERACAO.size()
 
 	if _tempo_dash_restante > 0.0:
 		_tempo_dash_restante = max(_tempo_dash_restante - delta, 0.0)
@@ -99,6 +110,13 @@ func _physics_process(delta: float) -> void:
 		_atualizar_animacao(direcao, esta_correndo, false)
 
 	move_and_slide()
+
+func _travar_movimento_pela_animacao(nome_clipe: String) -> void:
+	animation_player.play(nome_clipe, -1.0, velocidade_ataque)
+
+	var duracao_clipe: float = animation_player.get_animation(nome_clipe).length / velocidade_ataque
+	_tempo_ataque_restante = duracao_clipe
+	_tempo_movimento_travado_ataque_restante = duracao_clipe + folga_pos_golpe
 
 func _atualizar_animacao(direcao: Vector3, esta_correndo: bool, esta_dando_dash: bool) -> void:
 	var animacao_alvo: String = "idle"
