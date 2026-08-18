86bd82a5e9887d5cd16518031d24469f0c8f5957
anthyer <antonio_marcosph@hotmail.com>
2026-08-18 19:14:27 -0300

    feat(farming): adiciona indicador visual do quadrado alvo da ferramenta

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -15,6 +15,8 @@ const CLIPES_COMBO_ATAQUE: Array[String] = ["attack-melee-left", "attack-melee-l
 
 @onready var personagem: Node3D = $Personagem
 @onready var animation_player: AnimationPlayer = $Personagem/AnimationPlayer
+@onready var grade_solo: GradeSolo = $"../GradeSolo"
+@onready var indicador_alvo: MeshInstance3D = $"../IndicadorAlvo"
 
 var _tempo_dash_restante: float = 0.0
 var _tempo_cooldown_restante: float = 0.0
@@ -35,6 +37,14 @@ func _physics_process(delta: float) -> void:
 
 	if _tempo_janela_combo_restante <= 0.0:
 		_indice_combo = 0
+
+	var ferramenta_equipada: Ferramenta = EquipmentManager.ferramenta_atual()
+	indicador_alvo.visible = ferramenta_equipada != null
+	if ferramenta_equipada != null:
+		var celula_alvo: Vector2i = grade_solo.obter_celula_alvo(global_position, personagem.rotation.y)
+		var posicao_local: Vector3 = grade_solo.map_to_local(Vector3i(celula_alvo.x, 0, celula_alvo.y))
+		indicador_alvo.global_position = grade_solo.global_transform * posicao_local
+		indicador_alvo.global_position.y = 0.03
 
 	if _tempo_dash_restante <= 0.0 and _tempo_movimento_travado_ataque_restante <= 0.0 and _tempo_cooldown_restante <= 0.0 and InputManager.dash_pressionado():
 		_direcao_dash = Vector3(sin(personagem.rotation.y), 0.0, cos(personagem.rotation.y))
